521b917d75f44bd77030207fb4431f662c00c575
tomoyaueno29 <u.t012807@gmail.com>
2020-06-26 15:18:14 +0900

    Update lexer.go

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,9 +2,9 @@ package lexer
 
 type Lexer struct {
 	input        string
-	position     int
-	readPosition int
-	ch           byte
+	position     int  //入力に卒kる現在の位置 (現在の文字を指し示す)
+	readPosition int  //これから読み込む位置(現在の次の位置)
+	ch           byte //現在検査中の文字
 }
 
 func New(input string) *Lexer {
