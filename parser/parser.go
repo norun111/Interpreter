58e0bbbddad0762cf160e79959533172586cbed1
tomoyaueno29 <u.t012807@gmail.com>
2020-07-10 17:03:27 +0900

    Update parser.go

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,31 @@
 package parser
+
+import (
+	"goInterpreter/ast"
+	"goInterpreter/lexer"
+	"goInterpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken token.Token //現在のトークン
+	peekToken token.Token //次のトークン
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+
+}
