358149cdc2096eaa4c19b088b7f228e582813452
tomoyaueno29 <u.t012807@gmail.com>
2020-07-10 17:21:28 +0900

    Create parser_test.go

---
+++ parser/parser_test.go
@@ -0,0 +1,53 @@
+package parser
+
+import (
+	"goInterpreter/ast"
+	"goInterpreter/lexer"
+	"testing"
+)
+
+func TestLetStatement(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("PArsePrigram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statement does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letSum, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+}
