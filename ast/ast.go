59cb649aa83498b281c6747304f028c90215eabd
tomoyaueno29 <u.t012807@gmail.com>
2020-07-10 16:49:55 +0900

    Update ast.go

--- ast/ast.go
+++ ast/ast.go
@@ -22,13 +22,13 @@ type Program struct {
 
 type LetStatement struct {
 	Token token.Token
-	Name *Identifier
+	Name  *Identifier
 	Value Expression
 }
 
 func (ls *LetStatement) statement() {}
 
-func (ls *LetStatement) TokenLiteral() string {return ls.Token.Literal}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 type Identifier struct {
 	Token token.Token
@@ -37,7 +37,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
