bc38b4176a72011ffce4cd456901ff48e18498d1
tomoyaueno29 <u.t012807@gmail.com>
2020-07-11 19:58:27 +0900

    Update parser.go

--- parser/parser.go
+++ parser/parser.go
@@ -4,17 +4,18 @@ import (
 	"goInterpreter/ast"
 	"goInterpreter/lexer"
 	"goInterpreter/token"
+	"fmt"
 )
 
 type Parser struct {
 	l *lexer.Lexer
-
+	errors []string
 	curToken token.Token //現在のトークン
 	peekToken token.Token //次のトークン
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 	p.nextToken()
 	p.nextToken()
 
@@ -52,12 +53,14 @@ func (p *Parser) parseStatement() ast.Statement {
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
 
+	//初めに識別子を期待する
 	if !p.expectPeek(token.IDENT) {
 		return nil
 	}
 
 	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 
+	//次に等号を期待する　=
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
@@ -82,8 +85,19 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 
